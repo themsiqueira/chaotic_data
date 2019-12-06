0a2096ff8a737495a94d949313216de6dc5c0b6d
themsiqueira <mateus.carneiro@agrotools.com.br>
2019-12-06 09:55:08 -0300

    Update configs

--- src/app/controllers/ChaoticController.js
+++ src/app/controllers/ChaoticController.js
@@ -1,12 +1,28 @@
-
-
+import path from 'path';
+import fs from 'fs';
 
 class ChaoticController {
   async getJsonFileToJson(){
+    const filePath = path.join(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'tmp',
+      'chaotic_data.json'
+    );
 
+    const rawData = fs.readFileSync(filePath);
+    const result = JSON.parse(rawData);
+
+    return result;
   }
 
   async orderByStatus(req, res) {
+    const result = this.getJsonFileToJson();
+
+
+
     return
   }
 
