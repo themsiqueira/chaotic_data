d5b107544ced9b54c1a2702d9828569fbe571560
Mateus Siqueira Carneiro <mateussiqueiracarneiro@outlook.com>
2019-12-05 22:07:07 -0300

    first commit

---
+++ src/app/controllers/ChaoticController.js
@@ -0,0 +1,30 @@
+
+
+
+class ChaoticController {
+  async getJsonFileToJson(){
+
+  }
+
+  async orderByStatus(req, res) {
+    return
+  }
+
+  async getTotalByStatus(req, res) {
+    return
+  }
+
+  async orderByMajorValue(req, res) {
+
+    return
+  }
+
+  async groupByCountry(req, res) {
+
+
+    return
+  }
+
+}
+
+export default new ChaoticController();
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+
+import ChaoticController from './app/controllers/ChaoticController';
+
+const routes = new Router();
+
+routes.get('/api/v2/order-by-status', ChaoticController.orderByStatus);
+
+routes.get('/api/v2/order-total/:status', ChaoticController.getTotalByStatus);
+
+routes.get('/api/v2/order-major-values', ChaoticController.orderByMajorValue);
+
+routes.get('/api/v2/group-by-country', ChaoticController.groupByCountry);
+
+export default routes;
